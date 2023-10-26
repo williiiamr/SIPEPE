e7cab287cf64cd1c32107eaf57b81a4a99911901
William Rusli <wrusli75@gmail.com>
2023-10-26 17:04:58 +0700

    dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,158 +1,99 @@
 @extends('layouts.presensi')
 @section('head')
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-    <meta http-equiv="Pragma" content="no-cache" />
-    <meta http-equiv="Expires" content="0" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <meta name="theme-color" content="#000000">
-    <title>Dashboard</title>
-    <meta name="description" content="Mobilekit HTML Mobile UI Kit">
-    <meta name="keywords" content="bootstrap 4, mobile template, cordova, phonegap, mobile, html" />
-    <link rel="icon" type="image/png" href={{ asset("assets/img/favicon.png") }} sizes="32x32">
-    <link rel="apple-touch-icon" sizes="180x180" href={{ asset("assets/img/icon/192x192.png") }}>
-    <link rel="stylesheet" href={{ asset("assets/css/style.css") }}>
-    <link rel="manifest" href="__manifest.json">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link rel="stylesheet" href= {{ asset("assets/css/style-dashboard.css") }} />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 @endsection
 
 @section('content')
-
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,600;1,400&display=swap" rel="stylesheet">
-<script src = "./assets/js/main.js" defer></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src = "./assets/js/sweetalert.js" defer></script>
-
-<!-- Image and text -->
-<nav class="navbar navbar-light" style="background-color: white;">
-    <a class="navbar-brand" href="#">
-        <img src="assets/img/sample/photo/digitalforte.png" width="auto" height="40px" alt="">
-    </a>
-    <a>
-        <div class="avatar1">
-            <div class="nama">
-                @if(!empty(Auth::guard('karyawan')->user()->foto))
-                @php
-                $path = Storage::url('public/uploads/karyawan/'.Auth::guard('karyawan')->user()->foto);
-                @endphp
-                    <img src="{{ url($path) }}" alt="avatar" class="imaged w32 rounded">
-                @else
-                    <img src="assets/img/sample/avatar/avatar1.jpg" alt="avatar" class="imaged w32 rounded">
-                @endif
-                <div class="dropdown">
-                    <button>
-                        {{ Auth::guard('karyawan')->user()->nama }}
-                    </button>
-                    <div class="arrow-down">
-                    </div>
-                    <div class="dropdown-content">
-                        <a href="/editprofile">Edit Profile</a>  
-                        <a href="/proseslogout" onclick="showSuccess()">Log out</a>
-                    </div>
-                </div>
-            </div>
+<div class="background rounded-bottom-5 position-absolute shadow"></div>
+<div class="container-xl position-relative gx-0">
+  <div class="header d-flex justify-content-between py-2">
+    <img width="100" height="100" class="img-fluid" src="{{ asset("assets/img/logo.png") }}" alt="" />
+    <div class="d-flex align-items-center mx-3">
+      <img class="mx-2 img-fluid" width="40" height="40" src="{{ asset("assets/img/profil.png") }}" alt="" /> 
+      <div class="dropdown">
+        <button>
+            {{ Auth::guard('karyawan')->user()->nama }}
+        </button>
+        <div class="arrow-down">
         </div>
-    </a>
-    
-</nav>
-
-<div class="center-element mt-5">
-    <div class="header-ucapan">
-        Hello, {{ Auth::guard('karyawan')->user()->nama }} !
+        <div class="dropdown-content">
+            <a href="/proseslogout" onclick="showSuccess()">Log out</a>
+        </div>
     </div>
-    <div class="header-deskripsi">
-        Temukan Semangat
     </div>
-    <div class="header-deskripsi1">
-        Dalam Setiap Langkahs Karirmu!
+  </div>
+  <div class="appeal bg-white rounded-5 shadow">
+    <div class="text-center fw-bolder h-25 pt-3 appeal-title">PASTIKAN</div>
+    <div class="w-100 d-flex justify-content-center align-items-center h-75">
+      <div class="text-center mx-3 d-flex flex-column justify-content-center align-items-center reminder">
+        <img width="35" height="35" src="{{ asset("assets/img/solar_gps-bold.png") }}" alt="" />
+        <div class="fw-medium">GPS Menyala dan berfungsi dengan baik</div>
+      </div>
+      <div class="text-center mx-3 d-flex flex-column justify-content-center align-items-center reminder">
+        <img width="35" height="35" src="{{ asset("assets/img/ion_glasses.png") }}" alt="" />
+        <div class="fw-medium">Lepas topi, kacamata hitam, ataupun masker</div>
+      </div>
+      <div class="text-center mx-3 d-flex flex-column justify-content-center align-items-center reminder">
+        <img width="35" height="35" src="{{ asset("assets/img/solar_gps-bold-2.png") }}" alt="" />
+        <div class="fw-medium">Gunakan lokasi kerja sebagai latar belakang</div>
+      </div>
     </div>
-
-
+  </div>
+  <div class="mt-4 w-100 status-container d-flex justify-content-between gx-0">
+    <div class="status bg-danger rounded-4 d-flex flex-column justify-content-center shadow">
+      <div class="small-font fw-bolder ms-3 text-white">Absen Masuk</div>
+      <div class="medium-font fw-bolder ms-3 text-white"><span>{{ $presensihariini != null ? $presensihariini->jam_in : 'Anda Belum Absen' }}</span></div>
+    </div>
+    <div class="status bg-danger rounded-4 d-flex flex-column justify-content-center shadow">
+      <div class="small-font fw-bolder ms-3 text-white">Absen Pulang</div>
+      <div class="medium-font fw-bolder ms-3 text-white">{{ $presensihariini != null ? $presensihariini->jam_out : 'Anda Belum Absen' }}</div>
+    </div>
+  </div>
+  <div class="small-font fw-bolder my-4">
+    <p class="subtitle">Absensi Bulan <span class="purple">Maret</span></p>
+  </div>
+  <div class="row gx-0 info-row mb-5">
+    <div class="col-6 col-md-3 info-container">
+      <div class="h-100 bg-white information rounded-4 shadow px-md-4 px-3 d-flex justify-content-start align-items-center">
+        <img width="30" height="30" src="{{ asset("assets/img/mingcute_exit-door-line.png") }}" alt="" />
+        <div class="ms-2 d-flex flex-column justify-content-center align-items-start">
+          <div class="fs-6 fw-bold">Hadir</div>
+          <div class="test fw-bold days">{{ $rekap->jmlhadir }} Hari</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-6 col-md-3 info-container d-flex justify-content-end justify-content-md-start">
+      <div class="h-100 bg-white information rounded-4 shadow px-md-4 px-3 d-flex justify-content-start align-items-center">
+        <img width="30" height="30" src="{{ asset("assets/img/iconamoon_profile-fill.png") }}" alt="" />
+        <div class="ms-2 d-flex flex-column justify-content-center align-items-start">
+          <div class="fs-6 fw-bold">Izin</div>
+          <div class="test fw-bold days">0 Hari</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-6 col-md-3 info-container mt-md-0 mt-3 d-flex justify-content-md-end justify-content-start">
+      <div class="h-100 bg-white information rounded-4 shadow px-md-4 px-3 d-flex justify-content-start align-items-center">
+        <img width="30" height="30" src="{{ asset("assets/img/clarity_sad-face-solid.png") }}" alt="" />
+        <div class="ms-2 d-flex flex-column justify-content-center align-items-start">
+          <div class="fs-6 fw-bold">Sakit</div>
+          <div class="test fw-bold days">0 Hari</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-6 col-md-3 info-container mt-md-0 mt-3 d-flex justify-content-end">
+      <div class="h-100 bg-white information rounded-4 shadow px-md-4 px-3 d-flex justify-content-start align-items-center">
+        <img width="30" height="30" src="{{ asset("assets/img/mdi_clock-outline.png") }}" alt="" />
+        <div class="ms-2 d-flex flex-column justify-content-center align-items-start">
+          <div class="fs-6 fw-bold">Terlambat</div>
+          <div class="test fw-bold days">{{ $rekap->jmlterlambat }} Hari</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
-
-<div class="datetime">
-    <div class="time"></div>
-    <div class="date"></div>
-</div>
-
-
-
-<div class="rectangle-dashboard">
-    <div class="center-element">
-    <div class="card-rectangle1 mt-5 bg-success">
-        <div class="teks-presensi">
-                Masuk :
-            <div class="info-presensi">
-             <span>{{ $presensihariini != null ? $presensihariini->jam_in : 'Anda Belum Absen' }}</span>
-            </div>
-        </div>
-    </div>
-    <div class="card-rectangle2 mt-5 bg-danger">
-        <div class="teks-presensi">
-                Keluar :
-            <div class="info-presensi">
-             <span>{{ $presensihariini != null ? $presensihariini->jam_out : 'Anda Belum Absen' }}</span>
-            </div>
-        </div>
-    </div>
-    
-    </div>
-    <div class="center-element">
-    <div class="rectangle-guide">
-        <div class="isi-guide">
-            Pastikan:
-        </div>
-        <div class="container text-center">
-        <div class="row flex-wrap">
-    <div class="col mt-2">
-        <img src="assets/img/sample/photo/gps-bold.png" alt="image-gps" class="img-fluid">
-        <div class="deskripsi-guide">
-            GPS Menyala dan berfungsi dengan basik
-        </div>
-    </div>
-    <div class="col mt-2">
-        <img src="assets/img/sample/photo/ion_glasses.png" alt="image-gps" class="img-fluid">
-        <div class="deskripsi-guide">
-            Lepas topi, kacamata hitam, ataupun masker
-        </div>
-    </div>
-    <div class="col mt-2">
-        <img src="assets/img/sample/photo/solar-gps.png" alt="image-gps" class="img-fluid">
-        <div class="deskripsi-guide">
-            Gunakan lokasi kerja sebagai latar belakang
-        </div>
-    </div>
-</div>
-
-</div>
-    </div>
-    </div>
-    <br>
-    <div class="center-element">
-        <div class="card-grid">
-            <div class="card" style="border-radius: 20px;">
-                <h2 class="card-title">Hadir</h2>
-                <div class="gambar-presensi">
-                <img src="assets/img/sample/photo/uil_entry.png" width="65px">
-                </div>
-                <p class="card-description">{{ $rekap->jmlhadir }} Hari</p>
-            </div>
-            <div class="card" style="border-radius: 20px;">
-                <h2 class="card-title">Telat</h2>
-                <div class="gambar-presensi">
-                <img src="assets/img/sample/photo/bell-ringing.png" width="65px">
-                </div>
-                <p class="card-description">{{ $rekap->jmlterlambat }} Hari</p>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
-@endsection('content')
+@endsection
