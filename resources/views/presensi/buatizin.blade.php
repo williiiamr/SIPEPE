42f856f3e82d33982426148116b79f30dcaca5ec
Ferdy <51710824+ferdym02@users.noreply.github.com>
2023-11-04 21:53:21 +0700

    Update buatizin.blade.php

--- resources/views/presensi/buatizin.blade.php
+++ resources/views/presensi/buatizin.blade.php
@@ -42,6 +42,9 @@
           <div class="col-md-3 col-5 position-relative">
             <label for="tgl_izin" class="form-label">Tanggal</label>
             <input type="text" id="tgl_izin" name="tgl_izin" class="form-control form-control-sm datepicker" placeholder="Tanggal">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar4 position-absolute date-logo" viewBox="0 0 16 16">
+                <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1H2zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V5z"/>
+            </svg>
             <!-- <input type="date" id="tgl_izin" name="tgl_izin" class="form-control form-control-sm" /> -->
           </div>
           <div class="col-md-3 col-5 ms-2">
